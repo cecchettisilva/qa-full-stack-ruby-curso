75dc2491f92dc0d4f9cf5648e4039a5498537c7b
William Cecchetti da Silva <william.silva@pampa.compasso>
2019-12-20 11:31:29 -0300

    Feat: New test of drag and drop

--- ruby/capybara/spec/drag_drop_spec.rb
+++ ruby/capybara/spec/drag_drop_spec.rb
@@ -4,8 +4,12 @@ describe "Drag and Drop", :dragdrop do
   end
   it "Homem aranha pertence ao time do Stark" do
     stark = find('div[class*="team-stark"] div')
+    cap = find('div[class*="team-cap"] div')
+
     spiderman = find('img[alt$="Aranha"]') # encontra a imagem que termina com "Aranha", usando Regex
     spiderman.drag_to stark
+    expect(stark).to have_css 'img[alt$="Aranha"]' # verifica se foi para o time Stark
+    expect(cap).not_to have_css 'img[alt$="Aranha"]' # verifica se não há mais a imagem na div, fazendo um double check
   end
   after(:each) do
     sleep 3
